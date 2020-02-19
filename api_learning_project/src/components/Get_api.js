a32d7237a430454b870fe4a7962aad30c0d51442
Kerllsname <kirillpotapovnow@gmail.com>
2020-02-19 17:52:02 +0300

    empoyers in console

--- api_learning_project/src/components/Get_api.js
+++ api_learning_project/src/components/Get_api.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
+
 const GetApi = () => {
-    return(
-        <h1>
-            <center>
-                    hi get
-            </center>
-        </h1>
+    return (
+        <div></div>
     );
+}
+
+
+let xhhtp = new XMLHttpRequest();
+  
+  
+xhhtp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        myFunction(this.responseText)
+      }
+}
+  
+  
+xhhtp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
+xhhtp.send();
+  
+  
+function myFunction(data) {
+      console.log(data)
 }
 
 export default GetApi;
--- api_learning_project/src/components/post_api.js
+++ api_learning_project/src/components/post_api.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const PostApi = () => {
     return(
-        <h1>
-            <center>
-                    hi post
-            </center>
-        </h1>
+        <div>
+            
+        </div>
     );
 }
 
