75a850625968a3a1afa61bbd1199e2321f08d02e
Kerllsname <kirillpotapovnow@gmail.com>
2020-02-27 01:07:14 +0300

    add table and full get request

--- api_learning_project/src/components/Get_api.js
+++ api_learning_project/src/components/Get_api.js
@@ -2,9 +2,12 @@ import React from 'react';
 import axios from 'axios';
 
 export default class PersonList extends React.Component {
+
   state = {
     people: []
   }
+
+
   componentDidMount() {
     axios.get(`http://dummy.restapiexample.com/api/v1/employees`)
       .then(res => {
@@ -13,11 +16,47 @@ export default class PersonList extends React.Component {
       })
   }
 
+
   render() {
     return (
-      <ol>
-        {this.state.people.map(person => <li>{person.employee_name}</li>)}
-      </ol>
+
+      <table cellSpacing="15px">
+        
+        <tr>
+          <td align="center">
+            Id:
+          </td>
+          <td align="center">
+            Name:
+          </td>
+          <td align="center">
+            Salary:
+          </td>
+          <td align="center">
+            Age:
+          </td>
+        </tr>
+
+        {this.state.people.map(person =>
+          <tr>
+            <td align="center">
+              {person.id}
+            </td>
+            <td align="center">
+              {person.employee_name}
+            </td>
+            <td align="center">
+              {person.employee_salary}$
+            </td>
+            <td align="center">
+              {person.employee_age}
+            </td>
+          </tr>
+        )}
+
+      </table>
     )
+
   }
+
 }
