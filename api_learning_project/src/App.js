da5c688f658c48e330b2be0d2462cf2085f7b418
Kerllsname <kirillpotapovnow@gmail.com>
2020-03-05 13:06:39 +0300

    Post request

--- api_learning_project/src/App.js
+++ api_learning_project/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PersonList from './components/Get_api.js';
-
+import PeopleList from './components/Post_api.js'
 
 function App() {
   return (
     <div>
       <PersonList />
+      <PeopleList />
     </div>
   );
 }
